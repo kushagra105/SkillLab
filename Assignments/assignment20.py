b9a87045beca4d44bf7700f850e6a6eb3ee73168
madhav0922 <madhav.sharma0922@gmail.com>
2018-08-18 12:00:41 +0530

    madhav 18/08/18

---
+++ Assignments/assignment20.py
@@ -0,0 +1,5 @@
+#Problem Description : Correct the logical error in the code given below.
+
+for i in range(50,80):
+    if i % 2 == 0:
+        print(i)
---
+++ Assignments/assignment22.py
@@ -0,0 +1,11 @@
+bill_id=1001
+customer_id=101
+bill_amount=199.99
+customer_name = "Ls"
+if((len(customer_name) >= 3) and (len(customer_name) <= 20)) is True:
+    print("bill_id: " ,bill_id)
+    print("customer_id: ",customer_id)
+    print("customer_name: ",customer_name)
+    print("bill_amount Rs",bill_amount)
+else :
+    print("Invalid customer name. Customer name should be between 3 and 20 characters.")    
