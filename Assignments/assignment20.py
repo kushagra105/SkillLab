d8ac650bc7000536618bfdfee9e623217f5fed1e
madhav0922 <madhav.sharma0922@gmail.com>
2018-08-19 22:02:16 +0530

    madhav 19/08/18

--- Assignments/assignment20.py
+++ Assignments/assignment20.py
@@ -1,5 +1,7 @@
 #Problem Description : Correct the logical error in the code given below.
-
+# The program should print even numbers between the range 50 to 80.
 for i in range(50,80):
     if i % 2 == 0:
         print(i)
+ #   else:
+  #      break      removing else part gave the correct answer   
