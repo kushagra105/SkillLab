329a524c725bb4a9edfebe32af5fc8fb3ba474fb
lalitbansal1295 <lalitbansal1295@gmail.com>
2018-08-20 19:17:11 +0530

    20-08-2018

---
+++ Assignments/assignment38.py
@@ -0,0 +1,10 @@
+# Write a program to show pass by refrence method
+def compare(phoenix_to_slc , phoenix_to_tampa):
+    if phoenix_to_slc > phoenix_to_tampa:
+        print ("SLC is far from Phoenix as compared to Tampa, Florida")
+    elif (phoenix_to_slc < phoenix_to_tampa):
+        print ("Tampa,Florida is far from Phoenix compared to SLC")
+    else :
+        print ("Both locations are equidistance from Phoenix")
+compare (1790, 506)
+compare (phoenix_to_tampa = 506, phoenix_to_slc = 1790)
