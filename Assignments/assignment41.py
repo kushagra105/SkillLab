3a0a3a877fa7c59bad8e452960c06665495c8613
lalitbansal1295 <lalitbansal1295@gmail.com>
2018-08-23 19:03:19 +0530

    23-08-2018

---
+++ Assignments/assignment41.py
@@ -0,0 +1,16 @@
+#Implement the solution to the Problem
+item_price = [1050, 2200, 8575, 485, 234, 150, 399]
+def price_max (item_price):
+    print ("Price of costliest item in the retail store : ", max(item_price))
+def average_price(item_price):
+    total_price = 0
+    for i in item_price :
+        total_price += i
+    average_price = total_price/len(item_price)
+    print ("Average price of items in the retail store : ", average_price)
+def display_sorted_price (item_price):
+    item_price.sort()
+    print ("Item price list in increasing order : ", item_price)
+price_max(item_price)
+average_price(item_price)
+display_sorted_price(item_price)
---
+++ Assignments/assignment47.py
@@ -0,0 +1,15 @@
+#Implement the solution to the Problem using Object Oriented Language
+class Customer:
+    def setcustomerid(self, id):
+        self.__customerid = id
+    def settelephoneno(self, teleno):
+        self.__telephoneno = teleno
+    def getcustomerid (self):
+        return self.__customerid
+    def gettelephoneno(self):
+        return self.__telephoneno
+custobj = Customer()
+custobj.setcustomerid(1001)
+custobj.settelephoneno(123456789)
+print ("Customer ID :",custobj.getcustomerid())
+print ("Telephone No : ",custobj.gettelephoneno())
