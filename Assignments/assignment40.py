728824aa5caedaa925799ade11caf0295c9bd273
kushagra105 <jaiswal.kushagra105@gmail.com>
2018-09-05 12:02:18 +0530

    pushkush12:02

---
+++ Assignments/assignment40.py
@@ -0,0 +1,9 @@
+text = "I am a Python Developer"
+text1 = "       I am also a java  Developer            "
+print(text.endswith("Developer"))
+print(text.upper())
+print(text.lower())
+print(text.replace("is","was"))
+print(text1.strip())
+print(text.find("a"))
+print(text.startswith("Developer"))
