5966012b2790de41c5668aea35b7b864be1c5862
kushagra105 <jaiswal.kushagra105@gmail.com>
2018-09-07 02:19:51 +0530

    pushkush12:0

---
+++ Assignments/assignment49.py
@@ -0,0 +1,18 @@
+class retail:
+    def __init__(self, id=100, name="Null", ):
+        self.id = id
+        self.name = name
+
+    def add_person(self,id,name):
+        self.id = self.id + 1
+        self.name = name
+        print("Succeefully added")
+    def print_person(self):
+        print(self.id)
+        print(self.name)
+p1=retail(1002,"Kushagra");
+p1.add_person(1002,"Kushagra")
+p1.print_person();
+p2=retail(1003,"Madhav");
+p2.add_person(1003,"Madhav")
+p2.print_person();
