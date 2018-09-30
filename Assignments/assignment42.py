d8b0aa7f5437303dc219ce64ee7cceb70df7bb20
madhav0922 <madhav.sharma0922@gmail.com>
2018-09-30 23:01:41 +0530

    madhav o/p with programs

--- Assignments/assignment42.py
+++ Assignments/assignment42.py
@@ -1,1 +1,21 @@
+# Assignment 42 : 
+
 empid=[1001,1002,1003,1004,1005]
+id=input("Enter employee id ")
+try:
+    id[2] = 1002
+except :
+    print("Already present")
+
+p_id=input("Which employee id you want to search? ")
+
+try:
+    print(empid[p_id])
+except:
+    print("Not present")
+
+# ===================OUTPUT====================
+# Enter employee id 1002
+# Already present
+# Which employee id you want to search? 5
+# Not present
